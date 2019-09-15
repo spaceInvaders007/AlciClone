441dfd32d2b9f188a34495ffa029e4f45448467e
Ignacio Senese <ignaciosenese@gmail.com>
2019-09-15 22:46:35 +0930

    all tables seeded

--- database/index.js
+++ database/index.js
@@ -8,125 +8,7 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-// connection.connect(function(err) {
-//   if (err) {
-//     return console.error("error: " + err.message);
-//   }
 
-//   let createPatients = `create table if not exists patients(
-//                         patientId INT NOT NULL UNIQUE AUTO_INCREMENT,
-//                         lastName VARCHAR (50) NOT NULL,
-//                         firstName VARCHAR (50) NOT NULL,
-//                         age INT (3) NOT NULL,
-//                         sex VARCHAR (1) NOT NULL,
-//                         PRIMARY KEY (patientId)
-//                       );`;
-
-//   connection.query(createPatients, function(err, results, fields) {
-//     if (err) {
-//       console.log(err.message);
-//     }
-//   });
-
-//   let createAreas = `create table if not exists areas(
-//                       areaId INT NOT NULL UNIQUE AUTO_INCREMENT,
-//                       areaName VARCHAR (70) NOT NULL,
-//                       PRIMARY KEY (areaId)
-//                     );`;
-
-//   connection.query(createAreas, function(err, results, fields) {
-//     if (err) {
-//       console.log(err.message);
-//     }
-//   });
-
-//   let createRecords = `create table if not exists records(
-//                         recordId INT NOT NULL UNIQUE AUTO_INCREMENT,
-//                         patientId INT NOT NULL,
-//                         doctorId INT NOT NULL,
-//                         description VARCHAR (500) NOT NULL,
-//                         FOREIGN KEY (patientId)
-//                             REFERENCES patients(patientId),
-//                         FOREIGN KEY (doctorId)
-//                             REFERENCES doctors(doctorId),
-//                         PRIMARY KEY (recordId)
-//                       );`;
-
-//   connection.query(createRecords, function(err, results, fields) {
-//     if (err) {
-//       console.log(err.message);
-//     }
-//   });
-
-//   let createDoctors = `create table if not exists doctors(
-//                         doctorId INT NOT NULL UNIQUE AUTO_INCREMENT,
-//                         lastName VARCHAR (50) NOT NULL,
-//                         firstName VARCHAR (50) NOT NULL,
-//                         areaId INT,
-//                         FOREIGN KEY (areaId)
-//                             REFERENCES areas(areaId),
-//                         PRIMARY KEY (doctorId)
-//                       );`;
-
-//   connection.query(createDoctors, function(err, results, fields) {
-//     if (err) {
-//       console.log(err.message);
-//     }
-//   });
-
-//   let createNurses = `create table if not exists nurses(
-//                         nurseId INT NOT NULL UNIQUE AUTO_INCREMENT,
-//                         lastName VARCHAR (50) NOT NULL,
-//                         firstName VARCHAR (50) NOT NULL,
-//                         PRIMARY KEY (nurseId)
-//                       );`;
-
-//   connection.query(createNurses, function(err, results, fields) {
-//     if (err) {
-//       console.log(err.message);
-//     }
-//   });
-
-//   let createBeds = `create table if not exists beds(
-//                       bedId INT NOT NULL UNIQUE AUTO_INCREMENT,
-//                       bedNumber INT (3) NOT NULL,
-//                       areaId INT,
-//                       patientId INT NOT NULL,
-//                       LOS INT (4),
-//                       FOREIGN KEY (areaId)
-//                          REFERENCES areas(areaId),
-//                       FOREIGN KEY (patientId)
-//                          REFERENCES patients(patientId),
-//                       PRIMARY KEY (bedId)
-//   );`;
-
-//   connection.query(createBeds, function(err, results, fields) {
-//     if (err) {
-//       console.log(err.message);
-//     }
-//   });
-
-//   let createResults = `create table if not exists results(
-//                         resultId INT NOT NULL UNIQUE AUTO_INCREMENT,
-//                         bedId INT,
-//                         studyType VARCHAR (10) NOT NULL,
-//                         FOREIGN KEY (bedId)
-//                           REFERENCES beds(bedId),
-//                         PRIMARY KEY (bedId)
-// );`;
-
-//   connection.query(createResults, function(err, results, fields) {
-//     if (err) {
-//       console.log(err.message);
-//     }
-//   });
-
-//   connection.end(function(err) {
-//     if (err) {
-//       return console.log(err.message);
-//     }
-//   });
-// });
 
 //PATIENTS TABLE
 const selectAllPatients = function(cb) {
@@ -192,6 +74,22 @@ const insertManyRecords = function(records, cb = err => {}) {
   connection.query(queryString, cb);
 };
 
+//BEDS TABLE
+
+const insertManyBeds = function(beds, cb = err => {}) {
+  let queryString =
+    "INSERT INTO beds (bedNumber, areaId, patientId, LOS) VALUES ";
+  let values = beds.map(
+    bed =>
+      `("${bed.bedNumber}", "${bed.areaId}", "${bed.patientId}", "${bed.LOS}")`
+  );
+  queryString = queryString.concat(values.join(", "), ";");
+  connection.query(queryString, cb);
+};
+
+
+
+
 // const selectAll = function(cb) {
 //   return connection.query("SELECT * FROM timers", cb);
 // };
@@ -255,7 +153,8 @@ module.exports = {
   insertManyNurses,
   insertManyDoctors,
   insertManyResults,
-  insertManyRecords
+  insertManyRecords,
+  insertManyBeds
   // selectAll,
   // insertOne,
   // readOne,
--- database/seeder.js
+++ database/seeder.js
@@ -1,14 +1,139 @@
 var faker = require("faker");
 const db = require("./index.js");
 
-let randomGender = function() {
-  let number = Math.floor(Math.random() * 5) + 5;
-  if (number % 2 === 0) {
-    return "m";
-  } else {
-    return "f";
+
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "alcidion"
+});
+
+
+
+connection.connect(function(err) {
+  if (err) {
+    return console.error("error: " + err.message);
   }
-};
+
+  let createPatients = `create table if not exists patients(
+                        patientId INT NOT NULL UNIQUE AUTO_INCREMENT,
+                        lastName VARCHAR (50) NOT NULL,
+                        firstName VARCHAR (50) NOT NULL,
+                        age INT (3) NOT NULL,
+                        sex VARCHAR (1) NOT NULL,
+                        PRIMARY KEY (patientId)
+                      );`;
+
+  connection.query(createPatients, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+
+  let createAreas = `create table if not exists areas(
+                      areaId INT NOT NULL UNIQUE AUTO_INCREMENT,
+                      areaName VARCHAR (70) NOT NULL,
+                      PRIMARY KEY (areaId)
+                    );`;
+
+  connection.query(createAreas, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+
+  let createRecords = `create table if not exists records(
+                        recordId INT NOT NULL UNIQUE AUTO_INCREMENT,
+                        patientId INT NOT NULL,
+                        doctorId INT NOT NULL,
+                        description VARCHAR (500) NOT NULL,
+                        FOREIGN KEY (patientId)
+                            REFERENCES patients(patientId),
+                        FOREIGN KEY (doctorId)
+                            REFERENCES doctors(doctorId),
+                        PRIMARY KEY (recordId)
+                      );`;
+
+  connection.query(createRecords, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+
+  let createDoctors = `create table if not exists doctors(
+                        doctorId INT NOT NULL UNIQUE AUTO_INCREMENT,
+                        lastName VARCHAR (50) NOT NULL,
+                        firstName VARCHAR (50) NOT NULL,
+                        areaId INT,
+                        FOREIGN KEY (areaId)
+                            REFERENCES areas(areaId),
+                        PRIMARY KEY (doctorId)
+                      );`;
+
+  connection.query(createDoctors, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+
+  let createNurses = `create table if not exists nurses(
+                        nurseId INT NOT NULL UNIQUE AUTO_INCREMENT,
+                        lastName VARCHAR (50) NOT NULL,
+                        firstName VARCHAR (50) NOT NULL,
+                        PRIMARY KEY (nurseId)
+                      );`;
+
+  connection.query(createNurses, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+
+  let createBeds = `create table if not exists beds(
+                      bedId INT NOT NULL UNIQUE AUTO_INCREMENT,
+                      bedNumber INT (3) NOT NULL,
+                      areaId INT,
+                      patientId INT NOT NULL,
+                      LOS VARCHAR (5),
+                      FOREIGN KEY (areaId)
+                         REFERENCES areas(areaId),
+                      FOREIGN KEY (patientId)
+                         REFERENCES patients(patientId),
+                      PRIMARY KEY (bedId)
+  );`;
+
+  connection.query(createBeds, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+
+  let createResults = `create table if not exists results(
+                        resultId INT NOT NULL UNIQUE AUTO_INCREMENT,
+                        bedId INT,
+                        studyType VARCHAR (10) NOT NULL,
+                        FOREIGN KEY (bedId)
+                          REFERENCES beds(bedId),
+                        PRIMARY KEY (bedId)
+);`;
+
+  connection.query(createResults, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+
+  connection.end(function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+  });
+});
+
+
+
 
 // var patients = [
 //   {
@@ -33,7 +158,15 @@ let randomGender = function() {
 
 var seedPatients = function() {
   let patients = [];
-  for (i = 0; i < 230; i++) {
+  let randomGender = function() {
+    let number = Math.floor(Math.random() * 5) + 5;
+    if (number % 2 === 0) {
+      return "m";
+    } else {
+      return "f";
+    }
+  };
+  for (let i = 0; i < 230; i++) {
     patients.push({
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
@@ -54,7 +187,7 @@ var seedPatients = function() {
 
 var seedNurses = function() {
   let nurses = [];
-  for (i = 0; i < 50; i++) {
+  for (let i = 0; i < 50; i++) {
     nurses.push({
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName()
@@ -73,7 +206,7 @@ var seedNurses = function() {
 
 var seedDoctors = function() {
   let doctors = [];
-  for (i = 0; i < 50; i++) {
+  for (let i = 0; i < 50; i++) {
     doctors.push({
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
@@ -127,7 +260,7 @@ var seedResults = function() {
 
 var seedRecords = function() {
   let records = [];
-  for (i = 0; i < 490; i++) {
+  for (let i = 0; i < 490; i++) {
     records.push({
       patientId: Math.floor(Math.random() * 300) + 1,
       doctorId: Math.floor(Math.random() * 50) + 1,
@@ -147,8 +280,33 @@ var seedRecords = function() {
   });
 };
 
-//seedPatients();
+var seedBeds = function() {
+  let beds = [];
+  for (let j = 1; j < 6; j ++){
+    for (let i = 1; i < 51; i++) {
+      beds.push({
+        bedNumber: i,
+        areaId: j,
+        patientId: Math.floor(Math.random() * 300) + 1,
+        LOS: `${Math.floor(Math.random() * 24) + 1}:${Math.floor(Math.random() * 59) + 0}`
+      });
+    }
+  }
+  db.insertManyBeds(beds, err => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log("succesfully seeded");
+      return;
+    }
+  });
+};
+
+
+// seedPatients();
 // seedDoctors();
 // seedNurses();
 // seedResults();
-//seedRecords();
+// seedRecords();
+// seedBeds();
