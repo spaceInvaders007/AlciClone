39893a2e349d722b0e1908e82118656d9cd1f7b1
Ignacio Senese <ignaciosenese@Ignacios-MacBook-Air.local>
2019-09-20 07:45:07 +0900

    final backend, first two columns of results rendered

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,155 +1,166 @@
-import React from 'react';
-import Rows from './Rows.jsx';
-import styled from 'styled-components';
-import { Link } from "@reach/router"
+import React from "react";
+import Rows from "./Rows.jsx";
+import styled from "styled-components";
+import { Link } from "@reach/router";
 
 class Results extends React.Component {
-    constructor (props) {
-      super(props);
-      this.state = {
-        displayResults : this.props.displayResults,
-        beds: [],
-        areaId: '1',
-        patients: [],
-        doctors: [],
-        area: ''
-        // areas: []
-      }
-    }
-  
-    async componentDidMount() {
-      try {
-        await fetch("/beds");
-        let response = await fetch("/beds");
-        let beds = await response.json();
-        this.setState({ beds });
-      } catch (err) {
-        console.error("Encountered error fetching beds", err);
-      }
-      try {
-        await fetch("/patients");
-        let response = await fetch("/patients");
-        let patients = await response.json();
-        this.setState({ patients });
-      } catch (err) {
-        console.error("Encountered error fetching patients", err);
-      }
-      try {
-        await fetch("/doctors");
-        let response = await fetch("/doctors");
-        let doctors = await response.json();
-        this.setState({ doctors });
-      } catch (err) {
-        console.error("Encountered error fetching doctors", err);
-      }
-      try {
-            await fetch("/areas/" + this.state.areaId);
-            let response = await fetch("/areas/" + this.state.areaId);
-            let area = await response.json();
-            this.setState({ area: area[0].areaName });
-          } catch (err) {
-            console.error("Encountered error fetching areas", err);
-          }
-      // try {
-      //   await fetch("/areas");
-      //   let response = await fetch("/areas");
-      //   let areas = await response.json();
-      //   this.setState({ areas });
-      // } catch (err) {
-      //   console.error("Encountered error fetching areas", err);
-      // }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayResults: this.props.displayResults,
+      beds: [],
+      areaId: "1",
+      patients: [],
+      doctors: [],
+      area: ""
+      // areas: []
+    };
+  }
 
-    handleHomeClick = () => {
-      this.props.hideResultsDisplay()
+  async componentDidMount() {
+    try {
+      await fetch("/beds");
+      let response = await fetch("/beds");
+      let beds = await response.json();
+      this.setState({ beds });
+    } catch (err) {
+      console.error("Encountered error fetching beds", err);
     }
-    render () {
-      //   var rows = [];
-      // for (let i = 1; i < 51; i ++) {
-      //   rows.push (
-      //      <Rows number={i} key={i}/>
-      //     )
-      // }
-      return (
-        <div className="App">
-          <header className="App-header">
-          </header>
-          <Area>Area</Area>
-         <Filter>Filter</Filter>
-         <Sort>Sort</Sort>
-         <Set>Set</Set>
-         <Link to="/">
-         <Home onClick={this.handleHomeClick}>Home</Home>
-         </Link>
-          <BlueBar>
+    try {
+      await fetch("/patients");
+      let response = await fetch("/patients");
+      let patients = await response.json();
+      this.setState({ patients });
+    } catch (err) {
+      console.error("Encountered error fetching patients", err);
+    }
+    try {
+      await fetch("/doctors");
+      let response = await fetch("/doctors");
+      let doctors = await response.json();
+      this.setState({ doctors });
+    } catch (err) {
+      console.error("Encountered error fetching doctors", err);
+    }
+    try {
+      await fetch("/areas/" + this.state.areaId);
+      let response = await fetch("/areas/" + this.state.areaId);
+      let area = await response.json();
+      this.setState({ area: area[0].areaName });
+    } catch (err) {
+      console.error("Encountered error fetching areas", err);
+    }
+    // try {
+    //   await fetch("/areas");
+    //   let response = await fetch("/areas");
+    //   let areas = await response.json();
+    //   this.setState({ areas });
+    // } catch (err) {
+    //   console.error("Encountered error fetching areas", err);
+    // }
+  }
 
-          </BlueBar>
-          {/* <RowStyle > */}
-            {/* {rows} */}
+  handleHomeClick = () => {
+    this.props.hideResultsDisplay();
+  };
+  render() {
+    //   var rows = [];
+    // for (let i = 1; i < 51; i ++) {
+    //   rows.push (
+    //      <Rows number={i} key={i}/>
+    //     )
+    // }
+    return (
+      <div className="App">
+        <header className="App-header"></header>
+        <Area>Area</Area>
+        <Filter>Filter</Filter>
+        <Sort>Sort</Sort>
+        <Set>Set</Set>
+        <Link to="/">
+          <Home onClick={this.handleHomeClick}>Home</Home>
+        </Link>
+        <BlueBar></BlueBar>
+        {/* <RowStyle > */}
+        {/* {rows} */}
 
-          {/* </RowStyle> */}
-          {/* <button onClick={this.handleResultsClick}>Results</button>
+        {/* </RowStyle> */}
+        {/* <button onClick={this.handleResultsClick}>Results</button>
           <Results style={{display: this.state.resultsDisplay}}/> */}
-          {this.state.beds.filter((bed) => {
-            return bed.areaId === Number(this.state.areaId)}).map((bed) => {
-             return  <Rows
-                bed={bed} 
-                number={bed.bedNumber} 
-                key={bed.bedId} 
-                patientAge={this.state.patients.filter((patient)=>{
-                 return patient.patientId === bed.patientId
-                }).map((patient)=> {
-                  return patient.age
-                })}
-                patientFirstName={this.state.patients.filter((patient)=>{
-                  return patient.patientId === bed.patientId
-                 }).map((patient)=> {
-                   return patient.firstName
-                 })}
-                 patientSex={this.state.patients.filter((patient)=>{
-                  return patient.patientId === bed.patientId
-                 }).map((patient)=> {
-                   return patient.sex
-                 })}
-                 patientLastName={this.state.patients.filter((patient)=>{
-                  return patient.patientId === bed.patientId
-                 }).map((patient)=> {
-                   return patient.lastName
-                 })}
-                doctor={this.state.doctors.filter((doctor)=> {
-                  return doctor.doctorId === bed.doctorId
-                }).map((doctor)=> {
-                  return doctor.lastName
-                })}
+        {this.state.beds
+          .filter(bed => {
+            return bed.areaId === Number(this.state.areaId);
+          })
+          .map(bed => {
+            return (
+              <Rows
+                bed={bed}
+                number={bed.bedNumber}
+                key={bed.bedId}
+                patientAge={this.state.patients
+                  .filter(patient => {
+                    return patient.patientId === bed.patientId;
+                  })
+                  .map(patient => {
+                    return patient.age;
+                  })}
+                patientFirstName={this.state.patients
+                  .filter(patient => {
+                    return patient.patientId === bed.patientId;
+                  })
+                  .map(patient => {
+                    return patient.firstName;
+                  })}
+                patientSex={this.state.patients
+                  .filter(patient => {
+                    return patient.patientId === bed.patientId;
+                  })
+                  .map(patient => {
+                    return patient.sex;
+                  })}
+                patientLastName={this.state.patients
+                  .filter(patient => {
+                    return patient.patientId === bed.patientId;
+                  })
+                  .map(patient => {
+                    return patient.lastName;
+                  })}
+                patientId={this.state.patients
+                  .filter(patient => {
+                    return patient.patientId === bed.patientId;
+                  })
+                  .map(patient => {
+                    return patient.patientId;
+                  })}
+                doctor={this.state.doctors
+                  .filter(doctor => {
+                    return doctor.doctorId === bed.doctorId;
+                  })
+                  .map(doctor => {
+                    return doctor.lastName;
+                  })}
                 area={this.state.area}
               />
-            })
-          }
-        </div>
-      );
-    }
-  
+            );
+          })}
+      </div>
+    );
   }
-  
+}
+
 export default Results;
 
+const BlueBar = styled.div`
+  height: 25px;
+  background-image: linear-gradient(#1a2e41, #253945);
+`;
 
-const BlueBar = styled.div `
-height: 25px;
-background-image: linear-gradient(#1A2E41,#253945);
-`
+const Area = styled.button``;
 
-const Area = styled.button `
-`
+const Filter = styled.button``;
 
-const Filter = styled.button `
-`
+const Sort = styled.button``;
 
-const Sort = styled.button `
-`
+const Set = styled.button``;
 
-const Set = styled.button `
-`
-
-const Home = styled.button `
-`
+const Home = styled.button``;
--- client/src/components/Rows.jsx
+++ client/src/components/Rows.jsx
@@ -1,84 +1,123 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 class Rows extends React.Component {
-    constructor (props) {
-      super(props);
-      this.state = {
-        area: ''
-      }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      area: ""
+    };
+  }
 
-    // async componentDidMount () {
-    //   try {
-    //     await fetch("/areas/" + this.state.areaId);
-    //     let response = await fetch("/areas/" + this.state.areaId);
-    //     let area = await response.json();
-    //     this.setState({ area });
-    //   } catch (err) {
-    //     console.error("Encountered error fetching areas", err);
-    //   }
-    // }
-  
+  // async componentDidMount () {
+  //   try {
+  //     await fetch("/areas/" + this.state.areaId);
+  //     let response = await fetch("/areas/" + this.state.areaId);
+  //     let area = await response.json();
+  //     this.setState({ area });
+  //   } catch (err) {
+  //     console.error("Encountered error fetching areas", err);
+  //   }
+  // }
 
-    // async handleDelete(id) {
-    //   try {
-    //     await fetch("/timers/" + id, {
-    //       method: "DELETE",
-    //       body: JSON.stringify({ id })
-    //     });
-    //   } catch (err) {
-    //     console.log(err, "Couldn't delete one timer");
-    //   }
-    // }
+  // async handleDelete(id) {
+  //   try {
+  //     await fetch("/timers/" + id, {
+  //       method: "DELETE",
+  //       body: JSON.stringify({ id })
+  //     });
+  //   } catch (err) {
+  //     console.log(err, "Couldn't delete one timer");
+  //   }
+  // }
 
-
-    render () {
-    const {LOS, areaId, } = this.props.bed;
+  render() {
+    const { LOS, areaId } = this.props.bed;
     //const {doctor} = this.props.doctor
-  //   let patient = this.props.patient[0];
-  //  let doctor = this.props.doctor[0]
- //console.log(this.props.doctor[0])
+    //   let patient = this.props.patient[0];
+    //  let doctor = this.props.doctor[0]
+    //console.log(this.props.doctor[0])
     //console.log(patient.firstName)
-      return (
-        <div className="row">
-          <RowWrapper number={this.props.number}>
+    return (
+      <div className="row">
+        <RowWrapper number={this.props.number}>
           <AreaBed>
-            {this.props.area}
-            {this.props.bed.bedNumber}
+            <Area>{this.props.area}</Area>
+            <Bed>{this.props.bed.bedNumber}</Bed>
           </AreaBed>
-          <Patient>{this.props.doctor[0]}</Patient>
+          <Patient>
+            <PatientName>
+              <LastName>{this.props.patientLastName[0]},</LastName>
+              <FirstName>&nbsp; {this.props.patientFirstName[0]}</FirstName>
+            </PatientName>
+            <PatientIdAndDoctor>
+              <Urn>URN {this.props.patientId[0]}</Urn>
+              <Doctor>Dr {this.props.doctor[0]}</Doctor>
+            </PatientIdAndDoctor>
+          </Patient>
           <GenderAge></GenderAge>
           <Los></Los>
           <Results></Results>
-          </RowWrapper>
-        </div>
-      );
-    }
-  
+        </RowWrapper>
+      </div>
+    );
   }
-  
-  export default Rows;
-  
-  const RowWrapper = styled.div `
-   color: white; 
-   background: ${props => props.number % 2 === 0 ? '#383638' : '#232824'};
-   height: 50px;
-   `
-  
-  const AreaBed = styled.div `
-  
-  `
+}
 
-  const Patient = styled.div ``
+export default Rows;
 
-  const GenderAge = styled.div ``
+const RowWrapper = styled.div`
+  color: white;
+  background: ${props => (props.number % 2 === 0 ? "#383638" : "#232824")};
+  height: 50px;
+  display: flex;
+  align-items: center;
+`;
 
-  const Los = styled.div ``
+const AreaBed = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100px;
+`;
 
-  const Results = styled.div ``
+const Patient = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
+const GenderAge = styled.div``;
 
+const Los = styled.div``;
+
+const Results = styled.div``;
+
+const Area = styled.div`
+  margin: 0 auto;
+  margin-bottom: 8px;
+`;
+
+const Bed = styled.div`
+  margin: 0 auto;
+`;
+
+const FirstName = styled.div``;
+
+const LastName = styled.div``;
+
+const Urn = styled.div`
+  margin-right: 8px;
+`;
+
+const Doctor = styled.div``;
+
+const PatientName = styled.div`
+  display: flex;
+  margin-bottom: 8px;
+`;
+
+const PatientIdAndDoctor = styled.div`
+  display: flex;
+`;
 
 //   if (this.props.number % 2 === 0) {
 //     background-color: black
